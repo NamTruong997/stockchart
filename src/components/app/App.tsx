40d83a15af3fce83860fb865ffe192ee9d29c73a
Nam Truong <truongthanhnamdev@gmail.com>
2020-12-29 01:35:04 +0700

    Add more chart

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,21 +1,39 @@
-import { ChartDemo } from "components/chart-demo/ChartDemo";
+import { ChartDemo, ChartItemDataType } from "components/chart-demo/ChartDemo";
 import React, { useEffect, useState } from "react";
 import "./App.scss";
 const App: React.FC = () => {
-  const [data, setData] = useState<number[]>([]);
+  const [data, setData] = useState<ChartItemDataType[]>([]);
 
   useEffect(() => {
     let arrInput: number[] = [];
 
-    const length: any = prompt("Nhập số lượng phần tử mảng", "");
-    for (let i = 0; i < length; i += 1) {
+    // const length: any = prompt("Nhập số lượng phần tử mảng", "");
+    for (let i = 0; i < 700; i += 1) {
       // Random [0,-1,1]
       let numberRan = Math.floor(Math.random() * 3 + -1);
       arrInput.push(numberRan);
     }
 
-    setData(arrInput);
+    setData(formatData(arrInput));
   }, []);
+
+  //Chuyển đổi mảng input 0,1 => mảng theo type và giá trị
+  const formatData = (arr: number[], multiplier: number = 1): ChartItemDataType[] => {
+    let arrChart: ChartItemDataType[] = [];
+    let yTemp = 0;
+    let y1Temp = 0;
+    let y2Temp = 0;
+
+    return arr.reduce((arrChart, item, index) => {
+      let obj: ChartItemDataType = {
+        x: index * multiplier,
+        y: yTemp + item,
+      };
+      arrChart.push(obj);
+      yTemp += item;
+      return arrChart;
+    }, arrChart);
+  };
 
   return (
     <div className="com-home">
--- src/components/chart-demo/ChartDemo.tsx
+++ src/components/chart-demo/ChartDemo.tsx
@@ -1,60 +1,66 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import Chart, { Border, CommonAxisSettings, ConstantLine, Crosshair, Label, Legend, Pane, ScrollBar, Series, Tooltip, ValueAxis, ZoomAndPan } from "devextreme-react/chart";
+import Chart, { Border, CommonSeriesSettings, ConstantLine, Crosshair, Label, Legend, Point, ScrollBar, Series, Tooltip, ValueAxis, ZoomAndPan } from "devextreme-react/chart";
 import * as React from "react";
 import { useEffect, useState } from "react";
 
-export type ChartItemDataType = {
-  arg: number;
-  val: number;
-};
-
+/**
+ * TODO: Nên giữ lại define cũ: data: Array<{x: number, y: number}>.
+ */
 export interface ChartDemoProps {
-  data: number[];
+  data: ChartItemDataType[];
+  /**
+   * TODO: theo theme ở bên ngoài.
+   */
+  color?: {
+    firstLine: string;
+    secondLine: string;
+    thirdLine: string;
+    board: string;
+  };
+  theme?: string;
 }
-
-const MAX_SCROLL = 500;
 
 export const ChartDemo: React.FC<ChartDemoProps> = (props) => {
   const customizeTooltip = (pointInfo: any) => {
-    const data = pointInfo.point.data;
+    const data: ChartItemDataType = pointInfo.point.data;
     return {
-      text: `Giá trị: ${data.val}`,
+      text: `Giá trị: ${data.y}`,
     };
   };
   const [dataChart, setDataChart] = useState<ChartItemDataType[]>([]);
   const { data } = props;
 
-  //Chuyển đổi mảng input 0,1 => mảng theo type và giá trị
-  const formatData = (arr: number[], multiplier: number = 1): ChartItemDataType[] => {
-    let arrChart: ChartItemDataType[] = [];
-    let yTemp = 0;
-
-    return arr.reduce((arrChart, item, index) => {
-      let obj: ChartItemDataType = {
-        arg: index * multiplier,
-        val: yTemp + item,
-      };
-      arrChart.push(obj);
-      yTemp += item;
-      return arrChart;
-    }, arrChart);
-  };
-
   useEffect(() => {
-    setDataChart(formatData(data));
+    setDataChart(data);
   }, [data]);
 
   const onChartZoomEnd = (e: any) => {
     e.cancel = e.range.endValue - e.range.startValue < MAX_SCROLL;
   };
 
+  // const reduceData = (arr: ChartItemDataType[]) => {
+  //   let arrChart: ChartItemDataType[] = [];
+  //   return arr.reduce((arrChart, item, index) => {
+  //     if (arrChart[arrChart.length - 1]?.y !== item.y) arrChart.push(item);
+  //     return arrChart;
+  //   }, arrChart);
+  // };
+  console.log(dataChart);
   return (
     <>
       <Chart title="Biểu đồ phân tích " dataSource={dataChart} onZoomEnd={onChartZoomEnd}>
         <ScrollBar visible={true} />
-        <Pane name="bottom" />
-        <Series pane="bottom" />
-        <CommonAxisSettings endOnTick={false} />
+        {/* <Pane name="bottom" />
+        <Series pane="bottom" /> */}
+        <CommonSeriesSettings argumentField="x" type="line">
+          <Point visible={false}></Point>
+        </CommonSeriesSettings>
+        <Series valueField="y" name="Chart 1" />
+        {/* <Series valueField="y" name="Chart 2" />
+        <Series valueField="y" name="Chart 3" /> */}
+
+        {/* <ArgumentAxis argumentType="x" /> */}
+        {/* <ValueAxis position="right" /> */}
+        {/* <CommonAxisSettings endOnTick={false} /> */}
 
         <ValueAxis pane="bottom" title={{ text: "Chỉ số" }}>
           <ConstantLine width={3} value={0} color="#ff7c7c" dashStyle="dash" />
@@ -71,4 +77,13 @@ export const ChartDemo: React.FC<ChartDemoProps> = (props) => {
       </Chart>
     </>
   );
+};
+
+const MAX_SCROLL = 500;
+
+export type ChartItemDataType = {
+  x: number;
+  y: number;
+  y1?: number;
+  y2?: number;
 };
